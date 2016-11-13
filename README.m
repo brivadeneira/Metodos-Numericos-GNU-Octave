330b8f6a6286bc42240a97d2b429ae30dbb403d7
Bibiana Rivadeneira <brivadeneira@protonmail.com>
2016-11-13 11:00:15 -0300

    Update README.m

--- README.m
+++ README.m
@@ -1,7 +1,8 @@
 MÉTODOS NUMÉRICOS
-========================
+
 GNU/Octave
-Se implementan métodos numéricos para buscar una aproximaciones aceptables de valores como:
+
+Se implementan métodos numéricos para buscar aproximaciones aceptables de valores como:
   -Raíz/raíces de una función mediante métodos abiertos y cerrados.
   -Solución a sistemas de ecuaciones.
 
